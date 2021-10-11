82292f7eed69e2c56ed491a36806bca061409a20
fhilips <fhilips.souza@gmail.com>
2021-10-11 17:21:22 -0300

    GET Post by title, query methods

--- src/main/java/com/filipe/workshopmongo/models/controllers/PostController.java
+++ src/main/java/com/filipe/workshopmongo/models/controllers/PostController.java
@@ -1,10 +1,13 @@
 package com.filipe.workshopmongo.models.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.filipe.workshopmongo.models.dto.PostDTO;
@@ -22,4 +25,10 @@ public class PostController {
 		PostDTO user = service.findById(id);		
 		return ResponseEntity.ok().body(user);
 	}	
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<PostDTO>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
+		List<PostDTO> user = service.findByTitle(text);		
+		return ResponseEntity.ok().body(user);
+	}
 }
--- src/main/java/com/filipe/workshopmongo/services/PostService.java
+++ src/main/java/com/filipe/workshopmongo/services/PostService.java
@@ -1,6 +1,8 @@
 package com.filipe.workshopmongo.services;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,4 +27,10 @@ public class PostService {
 		Optional<Post> result = repository.findById(id);
 		return result.orElseThrow(() -> new ResourceNotFoundException("Usuário não encontrado!"));
 	}
+	
+	public List<PostDTO> findByTitle(String text) {
+		List<Post> list = repository.findByTitleContainingIgnoreCase(text);
+		return list.stream().map(x -> new PostDTO(x)).collect(Collectors.toList());
+	}
+	
 }
